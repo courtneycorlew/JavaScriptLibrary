c6b253926eebb05aabe29e85bc1b6d01a2ca6dec
CourtneyCorlew <courtneyecorlew@gmail.com>
2018-01-02 15:42:35 -0500

    Updated

--- 2.1/index.js
+++ 2.1/index.js
@@ -13,3 +13,18 @@ alert( `hello ${1}` ); // Outputs hello, 1
 alert( `hello ${"name"}` ); // Outputs hello, name
 
 alert( `hello ${name}` ); // Outputs hello, Ilya
+
+"" + 1 + 0  // "10"
+"" - 1 + 0 // -1
+true + false // 1
+6 / "3" // 2
+"2" * "3" //6
+4 + 5 + "px" // "9px"
+"$" + 4 + 5 // "$9"
+"4" - 2 // 2
+"4px" - 2 // NaN
+7 / 0 // Infinity
+"  -9\n" + 5 // " -9\n5"
+"  -9\n" - 5 //-14
+null + 1 //1
+undefined + 1 // NaN
